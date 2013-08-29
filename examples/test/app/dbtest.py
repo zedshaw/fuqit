acc871b7574ec5cd6e81e6e76ccc5f5d980e3977
Zed A. Shaw <zedshaw@zedshaw.com>
2013-08-29 16:31:59 -0700

    Example of deleting a record.

--- examples/test/app/dbtest.py
+++ examples/test/app/dbtest.py
@@ -5,6 +5,9 @@ def GET(web):
     import the db inside the .html file if you want and don't need
     to go to a handler first.
     """
+    if web.sub_path == '/delete':
+        web.db.delete('test', where='id = $id', vars=web.params)
+
     return web.app.render("showdb.html", web)
 
 def POST(web):
